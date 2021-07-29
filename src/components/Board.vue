d258a52e1c141feb6fe4252304ace93f8e21edce
Turu0512 <hinataptyan@gmail.com>
2021-07-30 07:38:49 +0900

    list表示まで完了　次削除の実装

---
+++ src/components/Board.vue
@@ -0,0 +1,37 @@
+<template>
+<div>
+ <header>
+  my Trello
+ </header>
+ <main>
+  <!-- タスクの総数 -->
+  <p class="info-line"> All:0 task</p>
+  <div class="list-index">
+   <list v-for="(item,index) in lists"
+   :key="item.id"
+   :title="item.title"
+   :listIndex="index"></list>
+  <list-add></list-add>
+  </div>
+ </main>
+</div>
+</template>
+
+<script>
+import ListAdd from "./ListAdd.vue"
+import List from "./List.vue"
+import { mapState } from "vuex"
+
+export default {
+ name:"board",
+ components: {
+    ListAdd,
+    List
+  },
+ computed: {
+  ...mapState([
+   'lists'
+  ]),
+ },
+}
+</script>
